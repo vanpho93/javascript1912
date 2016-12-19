ea89b94ead569e69c08aadca87e0a379a2b842ff
Admin <vanpho02@gmail.com>
2016-12-19 20:48:25 +0700

    start

---
+++ b1.js
@@ -0,0 +1,17 @@
+// // console.log('Hello world');
+// var a = 10;
+// b = 5;
+// a = 'Alo';
+// // console.log(a);
+// const c = 101;
+// let d = 100;
+// d = 'Hello';
+// let e = null;
+// console.log(typeof e);
+//
+// var a = parseInt('1');
+// console.log(1 === '1');
+
+// console.log(isNaN('10d'));
+
+console.log(eval('10 + 10'));
