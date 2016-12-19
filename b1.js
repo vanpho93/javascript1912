9cded4349375e44f555f547613f04e9631cf9864
Admin <vanpho02@gmail.com>
2016-12-19 21:19:10 +0700

    ket thuc

--- b1.js
+++ b1.js
@@ -14,4 +14,38 @@
 
 // console.log(isNaN('10d'));
 
-console.log(eval('10 + 10'));
+// console.log(eval('10 + 10'));
+
+var a = 1;
+var b = 1;
+var c = true;
+var f = false;
+
+// if(a++ > 10 & b++ > 0){
+//   console.log('Dung');
+// }else{
+//   console.log('Sai');
+// }
+// console.log(a += 10);
+// console.log(b);
+
+// var g = f? 5: 10;
+// console.log(g);
+
+// var h = (a++, a * b);
+// console.log(h);
+
+// var i = '0';
+// if(i){
+//   console.log('Dung');
+// }else{
+//   console.log('Sai');
+// }
+var a = 1;
+switch (a) {
+  case 1:
+    console.log('Dung');
+    a++;
+  case 2:
+    console.log('Sai');
+}
